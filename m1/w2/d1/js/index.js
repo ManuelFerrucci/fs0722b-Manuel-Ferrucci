a9604a20f25fd2531df8b80d24e5a03b2a819098
ManuelFerrucci <manuferrucci@hotmail.it>
2022-10-31 16:56:40 +0100

    first commit

---
+++ m1/w2/d1/js/index.js
@@ -0,0 +1,35 @@
+alert('Ciao! Una breve domanda:')
+
+alert('Rispondi sinceramente!')
+
+var anni = prompt('Quanti anni hai?');
+console.log(anni)
+
+if(anni < 18){
+    
+    document.getElementById('index-h1').innerHTML = 'Sei minorenne, esci dal sito!'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.getElementById('testo').innerHTML = 'Prova'
