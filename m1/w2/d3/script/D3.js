ee888550918b9ab96f240e40c29fc58d6894e56d
ManuelFerrucci <manuferrucci@hotmail.it>
2022-11-02 17:41:03 +0100

    Upgrade2

--- m1/w2/d3/script/D3.js
+++ m1/w2/d3/script/D3.js
@@ -249,23 +249,57 @@ const me = {
   name: 'John',
   lastName: 'Doe',
   skills: ['javascript', 'html', 'css']
+
 }
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css']
+
+}
+
+me.city = 'Toronto';
+console.log(me);
+*/
+
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css']
+
+}
+
+delete me.lastName;
+console.log(me);
+*/
+
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css']
+
+}
+
+delete me.skills;
+console.log(me);
+*/
+
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
