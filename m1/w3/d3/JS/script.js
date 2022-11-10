530ae270849d6e8a2b79d5eef2e6785cee8c920a
ManuelFerrucci <manuferrucci@hotmail.it>
2022-11-10 09:29:30 +0100

    Upgrade comment

--- m1/w3/d3/JS/script.js
+++ m1/w3/d3/JS/script.js
@@ -1,14 +1,20 @@
+/*IMPORTANTE - IL CODICE JAVASCRIPT E' TUTTO COMMENTATO PERCHE' NON SAPEVO FARE IL
+CODICE DA SOLO. QUINDI QUELLO CHE VEDI E' QUELLO FATTO DA MICHELE.
+MA DEVO IMPARARE, QUINDI DI LASCIARTELO FUNZIONANTE COME SE L'AVESSI FATTO IO
+MI SEMBRAVA UNA PRESA IN GIRO VERSO ENTRAMBI.
+SE DECOMMENTI FUNZIONA TUTTO.*/
+
 
 
 /*document.querySelector('#tasto1')
 .addEventListener('click', function() {
 
-    let testoInserito = document.querySelector('#testo-task');                     //IMPORTANTE - IL CODICE JAVASCRIPT E' TUTTO COMMENTATO PERCHE' NON SAPEVO FARE IL
-                                                                                   //CODICE DA SOLO. QUINDI QUELLO CHE VEDI E' QUELLO FATTO DA MICHELE. 
-    let contenitore = document.createElement('div');                               //SE DECOMMENTI FUNZIONA TUTTO
-    contenitore.classList.add('da-fare');                                          //MA DEVO IMPARARE, QUINDI DI LASCIARTELO FUNZIONANTE COME SE L'AVESSI FATTO IO
+    let testoInserito = document.querySelector('#testo-task');                     
 
-    let span = document.createElement('span');                                     //MI SEMBRAVA UNA PRESA IN GIRO VERSO ENTRAMBI
+    let contenitore = document.createElement('div');                               
+    contenitore.classList.add('da-fare');                                          
+
+    let span = document.createElement('span');                                     
     span.textContent = testoInserito.value;
 
     let completato = document.createElement('button');
