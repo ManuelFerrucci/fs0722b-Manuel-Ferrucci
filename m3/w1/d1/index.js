13e97b9c2def1e9d0fa39bd7240e90e57740dfbf
ManuelFerrucci <manuferrucci@hotmail.it>
2022-12-19 16:15:32 +0100

    Typescript primo esercizio

---
+++ m3/w1/d1/index.js
@@ -0,0 +1,27 @@
+var numeroCasuale = (Math.floor(Math.random() * (100 - 1) + 1));
+console.log(numeroCasuale);
+var numeroFrancesco = 40;
+console.log(numeroFrancesco);
+var numeroMario = 60;
+console.log(numeroMario);
+//var numeroManuale: number = 50;
+function confrontaNumeri() {
+    if (numeroCasuale == numeroFrancesco) {
+        console.log('Il numero generato e quello di Francesco sono uguali');
+    }
+    else if (numeroCasuale == numeroMario) {
+        console.log('Il numero generato e quello di Mario sono uguali');
+    }
+    else {
+        if ((numeroCasuale != numeroFrancesco) && (Math.abs(numeroCasuale - numeroFrancesco) < Math.abs(numeroCasuale - numeroMario))) {
+            console.log('Il numero uscito non è quello di Francesco, ma si avvicina di più');
+        }
+        else if ((numeroCasuale != numeroMario) && (Math.abs(numeroCasuale - numeroFrancesco) > Math.abs(numeroCasuale - numeroMario))) {
+            console.log('Il numero uscito non è quello di Mario, ma si avvicina di più');
+        }
+        else {
+            console.log('Il numero uscito non è quello di Mario né di Francesco, ed il numero casuale è esattamente alla stessa distanza tra i loro');
+        }
+    }
+}
+confrontaNumeri();
