9794147dfaef8dc2bcecda3da4cde11329406d92
ManuelFerrucci <manuferrucci@hotmail.it>
2022-12-22 16:53:04 +0100

    Consegna finale

--- m3/w1/d4/slider.js
+++ m3/w1/d4/slider.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     var splide = new Splide('.splide', {
         perPage: 1,
-        gap: '1rem'
+        gap: '1rem',
+        autoplay: true
     });
     splide.mount();
 });
