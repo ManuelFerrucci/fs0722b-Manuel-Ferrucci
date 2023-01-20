c03ce67612c56786cec1194520e59aa4fae664a7
ManuelFerrucci <manuferrucci@hotmail.it>
2023-01-20 17:13:51 +0100

    Esercizio Netflix

---
+++ m3/w3/d5/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { MoviesComponent } from './components/movies/movies.component';
+import { UserComponent } from './components/user/user.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
